6f90a64daefb2521c2b7c188d30108a91c305d30
raul victor <raulvictormello@hotmail.com>
2022-10-03 20:56:37 -0300

    arrumando erro

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
+
+
 const itens= [
     {
         id:0,
         nome:"sapato",
-        img:"no-imagem",
+        img: 'imagem.jpg',
         quantidade:0
     },
     {
         id:1,
         nome:"camiseta",
-        img:"no-imagem.jpg",
+        img:"imagem.jpg",
         quantidade: 0
     },
 
     {
         id:2,
         nome:"bone",
-        img:"no-imagem.jpg",
+        img:"imagem.jpg",
         quantidade:0
     },
 
     {
         id:3,
         nome:"celular",
-        img:"no-imagem.jpg",
+        img:"imagem.jpg",
         quantidade:0
     },
+    
 
 ]
 
@@ -32,14 +35,17 @@ function iniciar (){
  let conteinerprodutos = document.getElementById('container');
      itens.map ((tudo)=>{
      conteinerprodutos.innerHTML+=`
-        <div class="produto-singla"/> 
-        img scr "`+tudo.img+`" />
+        <div class="produto-singla"> 
+        <img src 
+        ="`+tudo.img+`">
+       
         <p> `+tudo.nome+` </p>
         <a key = "`+tudo.id+`" href=""> adicionar ao carrinho </a>
         </div>
     `;
    
  });
+ 
 }
 iniciar ()
  
